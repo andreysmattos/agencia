b3bfc22343a20ee7656a3ffd0f903136b439db1c
Andrey Mattos <andreysmattos@hotmail.com>
2019-08-21 02:43:05 -0300

    base css e tipografia

--- projeto/resources/views/components.blade.php
+++ projeto/resources/views/components.blade.php
@@ -1,3 +1,9 @@
 @extends('layouts.app')
 
-EAE RAPA
+
+<h2>Contact Us</h2>
+<h1> Get in Touch </h1>
+
+<p>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores odit incidunt beatae mollitia maiores error neque rem ipsa tenetur, sequi iste minima magnam, quis fuga! Quibusdam quo recusandae aut, aperiam.
+</p>
