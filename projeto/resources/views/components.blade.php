ddb75e0e684eb2817b6bc89c3eae1bc7080c49f1
Andrey Mattos <andreysmattos@hotmail.com>
2019-08-21 03:07:08 -0300

    botões

--- projeto/resources/views/components.blade.php
+++ projeto/resources/views/components.blade.php
@@ -1,9 +1,67 @@
 @extends('layouts.app')
 
 
-<h2>Contact Us</h2>
-<h1> Get in Touch </h1>
+<div class="container">
 
-<p>
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores odit incidunt beatae mollitia maiores error neque rem ipsa tenetur, sequi iste minima magnam, quis fuga! Quibusdam quo recusandae aut, aperiam.
-</p>
+	<br>
+	<br>
+	<br>
+
+
+	<h1>Tipografia</h1>
+
+	<br>
+	<br>
+	<br>
+
+	<h2>Contact Us</h2>
+	<h1> Get in Touch </h1>
+
+	<p>
+		Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores odit incidunt beatae mollitia maiores error neque rem ipsa tenetur, sequi iste minima magnam, quis fuga! Quibusdam quo recusandae aut, aperiam.
+	</p>
+
+
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+
+
+	<h1>Botões</h1>
+
+	<br>
+	<br>
+	<br>
+
+	<a href="#" class="button button__orange">More About us</a>
+
+
+
+
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+
+</div>
